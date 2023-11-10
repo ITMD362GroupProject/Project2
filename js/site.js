3c922a2427a0601dec187a5517bebd50f92479c4
mohammedhamzah95 <mohammedhamzah1995@gmail.com>
2023-11-10 02:32:46 -0600

    Add autofill for Contact Us form

--- js/site.js
+++ js/site.js
@@ -20,3 +20,17 @@ function redirectToShop() {
     // Replace the URL with the desired destination
     window.location.href = "https://thebigbeansevernapark.square.site/s/shop";
   }
+
+//Contact Us Form Autofill
+document.addEventListener("DOMContentLoaded", function () {
+
+  let storedName = localStorage.getItem("name");
+  let storedEmail = localStorage.getItem("email");
+  let storedSubject = localStorage.getItem("subject");
+  let storedMessage = localStorage.getItem("message");
+
+  if (storedName) document.getElementById("name").value = storedName;
+  if (storedEmail) document.getElementById("email").value = storedEmail;
+  if (storedSubject) document.getElementById("subject").value = storedSubject;
+  if (storedMessage) document.getElementById("message").value = storedMessage;
+});
