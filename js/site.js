c7fffc4cd4d951bc2cb2d608744d1acaf8118d41
MohammedOuannass <120182825+MohammedOuannass@users.noreply.github.com>
2023-11-09 21:21:15 -0600

    removing menu js

--- js/site.js
+++ js/site.js
@@ -16,18 +16,3 @@ function showSlides() {
 //end slidshow js
 
 // Menu expanding js
-document.addEventListener('DOMContentLoaded', function () {
-  const images = document.querySelectorAll('.menu-images-container img');
-
-  images.forEach(img => {
-    img.addEventListener('click', function () {
-      // Check if the screen width is less than or equal to 768px (typical mobile width)
-      const isMobile = window.innerWidth <= 768;
-
-      // Toggle the 'expanded' class only on mobile
-      if (isMobile) {
-        this.classList.toggle('expanded');
-      }
-    });
-  });
-});
